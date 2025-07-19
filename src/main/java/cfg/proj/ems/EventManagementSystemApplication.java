7e1b089f222b0c353c700c9a159d2fbb0b0bbd14
yesubabu2004 <2100030908cseh@gmail.com>
2025-07-19 16:30:33 +0530

    Add main for event creation functionality

--- src/main/java/cfg/proj/ems/EventManagementSystemApplication.java
+++ src/main/java/cfg/proj/ems/EventManagementSystemApplication.java
@@ -2,12 +2,19 @@ package cfg.proj.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cfg.proj"})
+@EntityScan("cfg.proj")
+@EnableJpaRepositories("cfg.proj")
 public class EventManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventManagementSystemApplication.class, args);
+	ConfigurableApplicationContext context=	SpringApplication.run(EventManagementSystemApplication.class, args);
+		
+		
 	}
 
 }

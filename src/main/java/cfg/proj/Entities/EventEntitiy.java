5d903a0467a7ebc5ef43888e25491a87f9309096
VARSHINI DOREDLA <110710638+varshini-10@users.noreply.github.com>
2025-07-21 12:18:44 +0530

    Add files via upload

---
+++ src/main/java/cfg/proj/Entities/EventEntitiy.java
@@ -0,0 +1,43 @@
+package cfg.proj.Entities;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(schema = "event",name="event")
+public class EventEntitiy {
+	
+	@Id
+	private int eventId;
+	
+	@Column
+	private String eventName;
+	
+	@Column
+	private String category;
+	
+	@Column
+	private String description;
+	
+	@Column
+	private int limit;
+	
+	@Column
+	private String location ;
+	
+	@Column
+	private LocalDate date;
+	
+	@OneToMany(mappedBy = "event")
+	private List<FeedbackEntity> feedbacks;
+	
+
+}

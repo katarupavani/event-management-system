f2980fc6c993e690c7f105916b887646642b0bfd
YESUBABU2004 <2100030908cseh@gmail.com>
2025-08-02 16:41:55 +0530

    Update Event.java

--- src/main/java/cfg/proj/DTO/Event.java
+++ src/main/java/cfg/proj/DTO/Event.java
@@ -1,18 +1,19 @@
 package cfg.proj.DTO;
 
 import java.time.LocalDate;
-
+import java.time.LocalTime;
 import lombok.Data;
 
 @Data
 public class Event {
-	
-	private int event_id;
-	private String event_name;
-	private String category;
-	private String description;
-	private int limit;
-	private String location;
-	private LocalDate date;
+    private int eventId;
+    private String eventName;
+    private String category;
+    private String description;
+    private int limit;
+    private String location;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
 
 }

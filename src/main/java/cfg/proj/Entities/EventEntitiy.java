64037f6bcf415aa4f6c438a887d4c785de4cce5d
YESUBABU2004 <2100030908cseh@gmail.com>
2025-08-02 16:43:35 +0530

    Update EventEntitiy.java

--- src/main/java/cfg/proj/Entities/EventEntitiy.java
+++ src/main/java/cfg/proj/Entities/EventEntitiy.java
@@ -1,10 +1,13 @@
 package cfg.proj.Entities;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -12,32 +15,37 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(schema = "event",name="event")
+@Table(schema = "event", name = "event")
 public class EventEntitiy {
-	
-	@Id
-	private int eventId;
-	
-	@Column
-	private String eventName;
-	
-	@Column
-	private String category;
-	
-	@Column
-	private String description;
-	
-	@Column
-	private int limit;
-	
-	@Column
-	private String location ;
-	
-	@Column
-	private LocalDate date;
-	
-	@OneToMany(mappedBy = "event")
-	private List<FeedbackEntity> feedbacks;
-	
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int eventId;
+
+    @Column
+    private String eventName;
+
+    @Column
+    private String category;
+
+    @Column
+    private String description;
+
+    @Column
+    private int limit;
+
+    @Column
+    private String location;
+
+    @Column
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+
+    @OneToMany(mappedBy = "event")
+    private List<FeedbackEntity> feedbacks;
 }

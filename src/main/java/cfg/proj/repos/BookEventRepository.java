885aa9e46bd6afea86b4bedad0a1b255cae6a726
Rajendra939098 <110449216+Rajendra939098@users.noreply.github.com>
2025-08-17 09:07:58 +0530

    Update BookEventRepository.java

--- src/main/java/cfg/proj/repos/BookEventRepository.java
+++ src/main/java/cfg/proj/repos/BookEventRepository.java
@@ -8,16 +8,26 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import cfg.proj.Entities.BookEventEntity;
+import cfg.proj.Entities.EventEntitiy;
+import cfg.proj.Entities.UserEntity;
 
 @Repository
 public interface BookEventRepository extends JpaRepository<BookEventEntity, Integer> {
 
-	@Query("from BookEventEntity b where b.user.userId =:userId")
-	List<BookEventEntity> findByUserId(int userId);
+ @Query("from BookEventEntity b where b.user.userId =:userId")
+ List<BookEventEntity> findByUserId(int userId);
 
-	@Query("from BookEventEntity b where b.event.eventId =:eventId")
-	List<BookEventEntity> findByEventId(int eventId);
+ @Query("from BookEventEntity b where b.event.eventId =:eventId")
+ List<BookEventEntity> findByEventId(int eventId);
 
-	List<BookEventEntity> findByEventDt(LocalDate date);
+ List<BookEventEntity> findByEventDt(LocalDate date);
+
+ boolean existsByEventAndUser(EventEntitiy event, UserEntity user);
+
+ long countByEvent(EventEntitiy event);
+
+ List<BookEventEntity> findByUser(UserEntity user);
+ 
+
 
 }
